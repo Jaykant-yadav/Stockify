5d3d451798903a7b83bd1f0cf057487869fe34a1
Jaykant Kumar <jaykant.yadav.cse@gmail.com>
2025-09-01 17:37:53 +0530

    SetUp Deshboard

---
+++ deshboard/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <>
+      <div>Apps</div>
+    </>
+  )
+}
+
+export default App
---
+++ deshboard/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import Topbar from './Topbar'
+import Deshboard from './Deshboard'
+
+function Home() {
+    return (
+        <>
+            <Topbar />
+            <Deshboard />
+        </>
+    )
+}
+
+export default Home
